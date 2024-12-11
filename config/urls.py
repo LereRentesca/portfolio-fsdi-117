774dc4f456c9943054176d7527f58d1e8b993cbc
LereRentesca <esteban.renteria@uabc.edu.mx>
2024-12-10 16:24:48 -0800

    Class #04

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("test_page", views.test_view, name="testing"),
     path("", views.about_view, name='about_url'),
+    path("content/", include('content.urls')),
 ]
